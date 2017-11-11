e78824b728e421f14195b7fd35bd6095f18924f0
sujeshvinod <sujeshvinod@gmail.com>
2017-11-11 20:23:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+    var content = "{'name':'Node JS Application 1'}";
+    var contentLength = content.length;
+    res.writeHead(200, {
+        'Content-Type':'application/json',
+        'Content-Length':contentLength,
+        'X-Server-Version':'3.0.0.7'
+    });
+    res.end(content);
+});
+server.listen(9091, function() {
+    console.log("Server listening on 9091....");
+});
